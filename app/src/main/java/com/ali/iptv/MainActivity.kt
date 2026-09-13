b6e6641e59476bff9400cff36e872eacf03d8e3d
alimoufeq3-max <alimoufeq3@gmail.com>
2026-09-13 18:54:04 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ali/iptv/MainActivity.kt
+++ app/src/main/java/com/ali/iptv/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.ali.iptv
 
+import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        val textView = TextView(this)
-        textView.text = "مرحباً بك في Ali IPTV"
-        textView.textSize = 24f
-        textView.setPadding(50, 50, 50, 50)
-        
+
+        val textView = TextView(this).apply {
+            text = "مرحباً بك في Ali IPTV"
+            textSize = 24f
+            setPadding(60, 60, 60, 60)
+        }
+
         setContentView(textView)
     }
 }
